12b08f9ada0e31fc6f4a33a77b0f7c7f71becf61
zachystuff <34614449+zachystuff@users.noreply.github.com>
2020-12-07 21:02:03 -0600

    project layout complete

---
+++ angular-project/src/app/shopping-list/shopping-added/shopping-added.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shopping-added',
+  templateUrl: './shopping-added.component.html',
+  styleUrls: ['./shopping-added.component.css']
+})
+export class ShoppingAddedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
