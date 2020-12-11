2067975bb3f03a61cfeeb2c45a0815bf16c54dd5
zachystuff <34614449+zachystuff@users.noreply.github.com>
2020-12-10 19:12:36 -0600

    recipe section fleshed out

--- angular-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ angular-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A test Recipe', 'This is simply a path', 'https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2015/11/shakshuka-11.jpg')
+  ];
 
   constructor() { }
 
