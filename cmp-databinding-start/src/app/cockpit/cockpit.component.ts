f0f0a8d7fe604fc1afaeb83211eddfbd63136ccb
zachystuff <34614449+zachystuff@users.noreply.github.com>
2020-12-22 10:31:02 -0600

    databinding lesson

---
+++ cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrls: ['./cockpit.component.css']
+})
+export class CockpitComponent implements OnInit {
+  @Output() serverCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
+  @Output() blueprintCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
+
+  // newServerName = '';
+  // newServerContent = '';
+  @ViewChild('serverContentInput') serverContentInput: ElementRef;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onAddServer(serverName: HTMLInputElement) {
+    this.serverCreated.emit({
+      serverName: serverName.value,
+      serverContent: this.serverContentInput.nativeElement.value
+    });
+  }
+
+  onAddBlueprint(serverName: HTMLInputElement) {
+   this.blueprintCreated.emit({
+     serverName: serverName.value,
+     serverContent: this.serverContentInput.nativeElement.value
+   })
+  }
+
+}
